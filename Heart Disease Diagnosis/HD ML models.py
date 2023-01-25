91b07e3fffd31366fd35e4df5024dd33e782b710
al <alr@ai-strategy.com>
2023-01-25 10:26:45 -0800

    gridsearch logistic regression

--- Heart Disease Diagnosis/HD ML models.py
+++ Heart Disease Diagnosis/HD ML models.py
@@ -6,7 +6,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
-
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import RepeatedStratifiedKFold
 
 #Loading Data
 heart_data = pd.read_csv('Data/Heart Disease Cleveland.csv')
@@ -31,3 +34,29 @@ predictions = LR_model.predict(X_test)
 metrics.accuracy_score(predictions, Y_test)
 metrics.precision_score(predictions, Y_test)
 metrics.recall_score(predictions, Y_test)
+
+
+
+#Random Forrest Hyperparamter Tuning
+
+# Create the model
+lrc = LogisticRegression()
+
+# define evaluation
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+# Define the hyperparameters and their possible values
+param_grid = {"C":[1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100], 
+              "penalty":['none', 'l1', 'l2', 'elasticnet'],
+              "solver":['newton-cg', 'lbfgs', 'liblinear']}
+
+
+# Create the GridSearchCV object
+grid_search = GridSearchCV(lrc, param_grid, scoring='accuracy', n_jobs= -1, cv=cv)
+
+# Fit the GridSearchCV object to the data
+grid_search.fit(X_train, Y_train)
+
+# Print the best parameters and score
+print("Best parameters: ", grid_search.best_params_)
+print("Best score: ", grid_search.best_score_)
